4944f78af70ac1046a5688e8f9d3447bba56387d
kajalgoyal14998@gmail.com <kajalgoyal14998@gmail.com>
2020-07-24 19:39:56 +0530

    created class customer and db connections

---
+++ FedChoice Bank/FedChoice Bank/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FedChoice_Bank.Models
+{
+    public class Customer
+    {
+        public int SSMid { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string address { get; set; }
+        public string state { get; set; }
+
+        public string city { get; set; }
+
+        public ICollection<Customer> Customers { get; set; }
+    }
+}
