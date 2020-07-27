92478bc095de687406cbd201b5a857956bbc093e
kajalgoyal14998@gmail.com <kajalgoyal14998@gmail.com>
2020-07-27 10:08:21 +0530

    data source changed

--- FedChoice Bank/FedChoice Bank/Models/LoginDbContext.cs
+++ FedChoice Bank/FedChoice Bank/Models/LoginDbContext.cs
@@ -22,7 +22,7 @@ namespace FedChoice_Bank.Models
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=FedChoiceBankDB;Integrated Security=True;");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-LTBMIDR\\SQLEXPRESS;Initial Catalog=FedChoiceBankDB;Integrated Security=True;");
             }
         }
 
